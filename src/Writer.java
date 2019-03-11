84c0e9fd0100512e1dfbcb991c7ad34beaa9af40
NinaLyalkina <34249585+NinaLyalkina@users.noreply.github.com>
2019-03-11 03:07:46 +0300

    Add files via upload

---
+++ src/Writer.java
@@ -0,0 +1,33 @@
+import java.io.*;
+
+public class Writer {
+    public Writer(String s) {
+
+    }
+
+    public static void main(String args[]) {
+        String s;
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Введите данные в файл. Для остановки записи нажмите Enter и введите 'stop'");
+
+        try {
+            FileWriter fw = new FileWriter("test.txt");
+
+
+            do {
+                System.out.print(": ");
+                s = br.readLine();
+
+                if (s.compareTo("stop") == 0) break;
+                fw.write(s);
+                fw.close();
+            } while (s.compareTo("stop") != 0);
+        } catch (IOException exc) {
+            System.out.println(exc);
+        }
+    }
+
+    private void write(String s) {
+    }
+}
+
