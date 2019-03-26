c52aab5fe100036e824e8d8305e93b2b0d404522
NinaLyalkina <34249585+NinaLyalkina@users.noreply.github.com>
2019-03-26 20:17:36 +0300

    Add files via upload

---
+++ src/excercise3.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class excercise3 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("введите текущий курс>>>");
+        double s = in.nextDouble();
+        System.out.println("введите количество рублей>>>");
+        double r = in.nextDouble();
+        double b = r / s;
+        double mul = Math.pow(10, 2);
+        System.out.println(((long)(b*mul))/mul);
+    }
+}
